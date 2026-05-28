74b1dcbfc6c7297ae0bbfe72b7030bd070790869
Jay Gitomer <Jay Gitomer>
2026-05-28 01:33:17 -0400

    Move headTags to top level

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,31 @@ const config = {
     locales: ['en'],
   },
 
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+      },
+    },
+  ],
+
   presets: [
     [
       'classic',
@@ -34,76 +59,15 @@ const config = {
     ],
   ],
 
-  themeConfig:
-    ({headTags: [
-  {
-    tagName: 'link',
-    attributes: {
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com',
-    },
-  },
-  {
-    tagName: 'link',
-    attributes: {
-      rel: 'preconnect',
-      href: 'https://fonts.gstatic.com',
-      crossorigin: 'anonymous',
-    },
-  },
- {
-    tagName: 'link',
-    attributes: {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
-    },
-  },
-],
-      navbar: {
-        title: '3D Print Shop Software',
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {
-            href: 'https://github.com/machinesandwords/3dprintshopsoftware',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Getting Started',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/machinesandwords/3dprintshopsoftware',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} 3D Print Shop Software.`,
-      },
-      prism: {
-        theme: require('prism-react-renderer').themes.github,
-        darkTheme: require('prism-react-renderer').themes.dracula,
-      },
-    }),
-};
-
-module.exports = config;
+  themeConfig: ({
+    navbar: {
+      title: '3D Print Shop Software',
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/machinesa
