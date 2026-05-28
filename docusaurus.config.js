523ab3f78a1e53d624d3cd12b195be6355b9509c
Jay Gitomer <Jay Gitomer>
2026-05-28 01:29:07 -0400

    Fix font href

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,11 +51,11 @@ const config = {
       crossorigin: 'anonymous',
     },
   },
-  {
+ {
     tagName: 'link',
     attributes: {
       rel: 'stylesheet',
-      https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap,
+      href: 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
     },
   },
 ],
